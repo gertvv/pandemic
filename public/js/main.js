ed1db282704bac771553a78ecfae5ab78f736d1c
Gert van Valkenhoef <g.h.m.van.valkenhoef@rug.nl>
2013-12-23 14:53:24 +0100

    Reindent

--- public/js/main.js
+++ public/js/main.js
@@ -1,64 +1,64 @@
 var app = angular.module('pandemic', []);
 
 app.factory('Board', function($http, $q) {
-	var dfd = $q.defer();
-	$http({method: 'GET', url: 'board.json'})
-		.success(function(data) {
-			console.log(data);
-			dfd.resolve(data);
-		})
-		.error(function(data) {
-			dfd.reject(data);
-		});
-	return dfd.promise;
+  var dfd = $q.defer();
+  $http({method: 'GET', url: 'board.json'})
+    .success(function(data) {
+      console.log(data);
+      dfd.resolve(data);
+    })
+    .error(function(data) {
+      dfd.reject(data);
+    });
+  return dfd.promise;
 });
 
 app.controller('ChatCtrl', function($scope) {
-	$scope.user = { 'name': '', 'type': 'user' };
-	$scope.text = '';
-	$scope.log = [];
-	$scope.log.push({
-		from: { 'name': 'Pandemic', 'type': 'system' },
-		text: 'Welcome to Pandemic!',
-		date: new Date()
-	});
+  $scope.user = { 'name': '', 'type': 'user' };
+  $scope.text = '';
+  $scope.log = [];
+  $scope.log.push({
+    from: { 'name': 'Pandemic', 'type': 'system' },
+    text: 'Welcome to Pandemic!',
+    date: new Date()
+  });
 
-	var host = window.document.location.host.replace(/:.*/, '');
-	var ws = 'ws://' + host + ':8080/messages';
-	var socket = io.connect(ws);
-	socket.on('chat', function(data) {
-		$scope.log.push(data);
-		$scope.$apply();
-	});
+  var host = window.document.location.host;
+  var ws = 'ws://' + host + '/messages';
+  var socket = io.connect(ws);
+  socket.on('chat', function(data) {
+    $scope.log.push(data);
+    $scope.$apply();
+  });
 
-	$scope.post = function(text) {
-		socket.emit('post', {
-			from: angular.copy($scope.user),
-			text: text
-		});
-		$scope.text = '';
-	};
+  $scope.post = function(text) {
+    socket.emit('post', {
+      from: angular.copy($scope.user),
+      text: text
+    });
+    $scope.text = '';
+  };
 });
 
 app.controller('MapCtrl', function($scope, Board) {
-	$scope.cities = [];
-	Board.then(function(data) {
-		$scope.cities = data.cities;
-	});
+  $scope.cities = [];
+  Board.then(function(data) {
+    $scope.cities = data.cities;
+  });
 });
 
 app.filter('reverse', function() {
-	return function(list) {
-		return list.slice().reverse();
-	};
+  return function(list) {
+    return list.slice().reverse();
+  };
 });
 
 app.filter('groupColor', function(Board) {
-	var colors = {};
-	Board.then(function(data) {
-		colors = _.object(_.map(data.groups, function(x) { return x.name; }), _.map(data.groups, function(x) { return x.color; }));
-	});
-	return function(group) {
-		return colors[group] || "white";
-	};
+  var colors = {};
+  Board.then(function(data) {
+    colors = _.object(_.map(data.groups, function(x) { return x.name; }), _.map(data.groups, function(x) { return x.color; }));
+  });
+  return function(group) {
+    return colors[group] || "white";
+  };
 });
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express'),
-	app = express(),
+  app = express(),
     server = require('http').createServer(app),
-	io = require('socket.io').listen(server);
+  io = require('socket.io').listen(server);
 
 app.use(express.static(__dirname + '/public'));
 
@@ -11,9 +11,9 @@ var errorLogger = function(error) { if (error) { console.log('ERROR:', error); }
 
 var chat = io.of('/messages')
   .on('connection', function(socket) {
-	socket.emit('chat', { from: { 'name': 'Pandemic', 'type': 'system' }, text: 'Welcome to Pandemic!', date: Date.now() });
-	socket.on('post', function(message) {
-		message.date = Date.now();
-		chat.emit('chat', message, errorLogger);
-	});
+  socket.emit('chat', { from: { 'name': 'Pandemic', 'type': 'system' }, text: 'Welcome to Pandemic!', date: Date.now() });
+  socket.on('post', function(message) {
+    message.date = Date.now();
+    chat.emit('chat', message, errorLogger);
+  });
 });
