2bcf09009d2eb6e43703e2eba08a2429b1c3ae42
Gert van Valkenhoef <g.h.m.van.valkenhoef@rug.nl>
2013-12-23 18:19:40 +0100

    User and game REST resources

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,9 @@
-var app = angular.module('pandemic', []);
+var app = angular.module('pandemic', ['ui.router', 'ngCookies']);
 
 app.factory('Board', function($http, $q) {
   var dfd = $q.defer();
   $http({method: 'GET', url: 'board.json'})
     .success(function(data) {
-      console.log(data);
       dfd.resolve(data);
     })
     .error(function(data) {
@@ -14,17 +13,11 @@ app.factory('Board', function($http, $q) {
 });
 
 app.controller('ChatCtrl', function($scope) {
-  $scope.user = { 'name': '', 'type': 'user' };
   $scope.text = '';
   $scope.log = [];
-  $scope.log.push({
-    from: { 'name': 'Pandemic', 'type': 'system' },
-    text: 'Welcome to Pandemic!',
-    date: new Date()
-  });
 
   var host = window.document.location.host;
-  var ws = 'ws://' + host + '/messages';
+  var ws = 'ws://' + host + $scope.game.ws;
   var socket = io.connect(ws);
   socket.on('chat', function(data) {
     $scope.log.push(data);
@@ -61,4 +54,85 @@ app.filter('groupColor', function(Board) {
   return function(group) {
     return colors[group] || "white";
   };
+});
+
+function HomeCtrl($scope, $http, $location, games) {
+  $scope.games = games.data;
+  $scope.createGame = function() {
+    $http({method: 'POST', url: '/games'})
+      .success(function(data) {
+        $location.path('/games/' + data.id);
+      });
+  };
+}
+
+app.config(function($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise('/home');
+
+  $stateProvider.state('root', {
+    template: '<div ui-view></div>',
+    resolve: {
+      user: function($q, $cookies, $http) {
+        var createUser = function(user) {
+          $http({method: 'POST', url: '/me'})
+            .success(function(data) {
+              $cookies.pandemicToken = data.token;
+              delete data.token;
+              user.resolve(data);
+            });
+        }
+
+        var user = $q.defer();
+        if ($cookies.pandemicToken) {
+          $http({method: 'GET', url: '/me'})
+            .success(function(data) {
+              user.resolve(data);
+            })
+            .error(function(error) {
+              createUser(user);
+            });
+        } else {
+          createUser(user);
+        }
+        return user.promise;
+      }
+    },
+    controller: function($scope, user, $http) {
+      $scope.user = user;
+      $scope.userNameUpdated = null;
+      $scope.setName = function(name) {
+        $http({method: 'PUT', url: '/me', data: { 'name': name }}).success(function() {
+          $scope.userNameUpdated = 'success';
+          setTimeout(function() { $scope.userNameUpdated = null; }, 2000);
+        }).error(function() {
+          $scope.userNameUpdated = 'error';
+          setTimeout(function() { $scope.userNameUpdated = null; }, 2000);
+        });
+      }
+    }
+  });
+
+  $stateProvider.state('root.home', {
+    url: '/home',
+    templateUrl: 'partials/landing.html',
+    resolve: {
+      games: function($http) {
+        return $http({method: 'GET', url: '/games'});
+      }
+    },
+    controller: HomeCtrl,
+  });
+
+  $stateProvider.state('root.game', {
+    url: '/games/:id',
+    templateUrl: 'partials/game.html',
+    resolve: {
+      game: function($http, $stateParams) {
+        return $http({method: 'GET', url: '/games/' + $stateParams.id});
+      }
+    },
+    controller: function($scope, game) {
+      $scope.game = game.data;
+    }
+  });
 });
--- server.js
+++ server.js
@@ -1,19 +1,101 @@
 var express = require('express'),
   app = express(),
-    server = require('http').createServer(app),
-  io = require('socket.io').listen(server);
+  server = require('http').createServer(app),
+  io = require('socket.io').listen(server),
+  cookie = require('cookie');
 
 app.use(express.static(__dirname + '/public'));
+app.use(express.cookieParser());
+app.use(express.bodyParser());
+
+function randomId(size, prefix) {
+  var text = "";
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for(var i = 0; i < size; i++ ) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return prefix ? prefix + text : text;
+}
+
+var userTokens = {};
+var users = {};
+
+app.get('/me', function(req, res) {
+  var token = req.cookies.pandemicToken;
+  if (!userTokens[token]) {
+    res.status(404);
+  } else {
+    var id = userTokens[token];
+    res.json(users[id]);
+  }
+  res.end();
+});
+
+app.post('/me', function(req, res) {
+  var token = randomId(16);
+  var id = randomId(5);
+  var user = { 'name': null, 'id': id, 'type': 'user' }; 
+  users[id] = user;
+  userTokens[token] = id;
+  res.json(201, { 'name': null, 'id': id, 'type': 'user', 'token': token });
+  res.end();
+});
+
+app.put('/me', function(req, res) {
+  var token = req.cookies.pandemicToken;
+  if (!userTokens[token]) {
+    res.status(403);
+  } else {
+    var id = userTokens[token];
+    console.log(users[id], req.body);
+    users[id].name = req.body.name;
+    res.json(users[id]);
+  }
+  res.end();
+});
+
+var games = {};
+
+app.get('/games', function(req, res) {
+  res.json(games);
+  res.end();
+});
+
+app.get('/games/:id?', function(req, res) {
+  var id = req.params.id;
+  if (games[id]) {
+    res.json(games[id]);
+    res.end();
+  } else {
+    res.status(404);
+    res.end();
+  }
+});
+
+function createWS(url) {
+  var chat = io.of(url)
+    .on('connection', function(socket) {
+      console.log();
+      var token = cookie.parse(socket.handshake.headers.cookie).pandemicToken;
+      var user = users[userTokens[token]];
+      socket.emit('chat', { from: { 'name': 'Pandemic', 'type': 'system' }, text: 'Welcome to Pandemic!', date: Date.now() });
+      socket.on('post', function(message) {
+        message.from = user;
+        message.date = Date.now();
+        chat.emit('chat', message, errorLogger);
+      });
+    });
+}
+
+app.post('/games', function(req, res) {
+  var id = randomId(5);
+  games[id] = { id: id, title: 'Pandemic Game', _self: '/games/' + id, ws: '/games/' + id };
+  createWS(games[id].ws);
+  res.json(201, games[id]);
+  res.end();
+});
 
 server.listen(8080);
 
 var errorLogger = function(error) { if (error) { console.log('ERROR:', error); } };
-
-var chat = io.of('/messages')
-  .on('connection', function(socket) {
-  socket.emit('chat', { from: { 'name': 'Pandemic', 'type': 'system' }, text: 'Welcome to Pandemic!', date: Date.now() });
-  socket.on('post', function(message) {
-    message.date = Date.now();
-    chat.emit('chat', message, errorLogger);
-  });
-});
